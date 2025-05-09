60e1ff0babc9fdafbc42e6bfb4ac5dd52e80ec55
emilianolee <yechan5172@daum.net>
2025-05-09 17:21:05 +0900

    2일차 커밋

---
+++ day02/Day02Wpf/WpfBasicApp01/Bootstrapper.cs
@@ -0,0 +1,32 @@
+﻿using Caliburn.Micro;
+using MahApps.Metro.Controls.Dialogs;
+using System.Windows;
+using WpfBasicApp01.ViewModels;
+
+namespace WpfBasicApp01
+{
+    public class Bootstrapper : BootstrapperBase
+    {
+        private SimpleContainer _container;
+
+        public Bootstrapper() 
+        {
+            Initialize();
+        }
+
+        protected override void Configure()
+        {
+            _container = new SimpleContainer();
+            _container.Singleton<WindowManager, WindowManager>();
+            _container.Singleton<IDialogCoordinator, IDialogCoordinator>();
+            _container.PerRequest<MainViewModel>();
+        }
+
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            //base.OnStartup(sender, e);
+            // App.xaml의 StratupUri와 동일한 일을 수행
+            DisplayRootViewForAsync<MainViewModel>();   // MainViewModel과 동일한 이름의 View를 찾아서 바인딩 후 실행
+        }
+    }
+}
---
+++ day02/Day02Wpf/WpfBasicApp01/ViewModels/MainViewModel.cs
@@ -0,0 +1,39 @@
+﻿using Caliburn.Micro;
+using MahApps.Metro.Controls.Dialogs;
+using System.Windows;
+
+namespace WpfBasicApp01.ViewModels
+{
+    public class MainViewModel : Conductor<object>
+    {
+        // 메시지박스, 다이얼로그를 위한 변수
+        private readonly IDialogCoordinator _dialogCoordinator;
+
+        private string _greeting;
+        public string Greeting
+        {
+            get => _greeting;
+            set
+            {
+                _greeting = value;
+                NotifyOfPropertyChange(() => Greeting);    // 속성 값 바뀐 것을 알려줘야 함
+            }
+        } 
+
+        public MainViewModel(IDialogCoordinator dialogCoordinator)
+        {
+            _dialogCoordinator = dialogCoordinator;
+            Greeting = "Hola, Caliburn.Micro!!";
+        }
+
+        public async void SayHello()
+        {
+            Greeting = "Hola a todos ^-^ ";
+
+            // WinForms 방식
+            //MessageBox.Show("Hola a todos ~ !!", "Greeting", MessageBoxButton.OK, MessageBoxImage.Information);
+            await _dialogCoordinator.ShowMessageAsync(this, "Greeting", "Hola a todos ~! ~!");
+
+        }
+    }
+}
